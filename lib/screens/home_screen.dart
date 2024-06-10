bb1e4c6af422785705edff65c79e8cdabd0757bd
lukaskiermeyer <lukas.kiermeyer@st.oth-regensburg.de>
2024-06-11 00:29:30 +0100

    Version 2: Safe and Load Marker in/aus Datenbank
    - Hinzufügen von Markern in Datenbank
    - Laden der Datenbankdaten in ein Array
    - Kotlin Version update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:mysql1/mysql1.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -11,20 +13,65 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final MapController _mapController = MapController();
-  List<Marker> _markers = [];
-  List<Marker> _currentMarker = [];
+  final List<Marker> _markers = [];
+  final List<Marker> _currentMarker = [];
+  late MySqlConnection _conn;
+
+  @override
+  void initState() {
+    super.initState();
+    _connectToDatabase();
+
+  }
+
+  Future<void> _connectToDatabase() async {
+    _conn = await MySqlConnection.connect(ConnectionSettings(
+      host: 'sql7.freesqldatabase.com',
+      port: 3306,
+      user: 'sql7712971',
+      db: 'sql7712971',
+      password: 'YnYC9zjPM1',
+    ));
+    _loadMarkersFromDatabase();
+  }
+
+  Future<void> _loadMarkersFromDatabase() async {
+    final results = await _conn.query('SELECT * FROM markers');
+
+    for (var row in results) {
+      _markers.add(
+        Marker(
+          point: LatLng(row[1], row[2]),
+          width: 30,
+          height: 30,
+          child: const Icon(Icons.location_pin),
+        ),
+      );
+    }
+    setState(() {});
+  }
+
+  Future<void> _saveMarkerToDatabase() async {
+    if (_currentMarker.isNotEmpty) {
+      await _conn.query(
+        'INSERT INTO markers (latitude, longitude) VALUES (?, ?)',
+        [_currentMarker[0].point.latitude,_currentMarker[0].point.longitude]
+      );
+      _loadMarkersFromDatabase();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Interactive World Map'),
+        title: const Text('Interactive World Map'),
         actions: [
           IconButton(
-            icon: Icon(Icons.menu),
+            icon: const Icon(Icons.menu),
             onPressed: () {
               // Handle menu button press
-              print("Menu button pressed");
+              
             },
           ),
         ],
@@ -35,8 +82,8 @@ class _MyHomePageState extends State<MyHomePage> {
             child: FlutterMap(
               mapController: _mapController,
               options: MapOptions(
-                initialCenter: LatLng(51.509364, -0.128928),
-                initialZoom: 9.2,
+                initialCenter: const LatLng(51.509364, -0.128928),
+                initialZoom: 4,
                 onTap: (point, latLng) {
                   setState(() {
                     if (_currentMarker.isNotEmpty) {
@@ -44,7 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         point: latLng,
                         width: 200,
                         height: 200,
-                        child: Icon(Icons.location_on_outlined),
+                        child: const Icon(Icons.location_on_outlined),
                       );
                     } else {
                       _currentMarker.add(
@@ -52,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           point: latLng,
                           width: 200,
                           height: 200,
-                          child: Icon(Icons.location_on_outlined),
+                          child: const Icon(Icons.location_on_outlined),
                         ),
                       );
                     }
@@ -76,23 +123,24 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (_currentMarker.isNotEmpty) {
-                      setState(() {
+                      setState(() async {
                         _currentMarker.add(
                           Marker(
                             point: _currentMarker.last.point,
                             width: 100,
                             height: 100,
-                            child: Icon(Icons.location_pin),
+                            child: const Icon(Icons.location_pin),
                           ),
                         );
-                        _markers.add(_currentMarker[1]);
+                        //sql query to insert marker
+                        await _saveMarkerToDatabase();
                         _currentMarker.clear();
                       });
                     }
                   },
-                  child: Text('Save Marker'),
+                  child: const Text('Save Marker'),
                 ),
                 ElevatedButton(
                   onPressed: () {
@@ -100,7 +148,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       _currentMarker.clear();
                     });
                   },
-                  child: Text('Cancel Marker'),
+                  child: const Text('Cancel Marker'),
                 ),
               ],
             ),
